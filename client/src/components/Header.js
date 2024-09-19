1f029b1accdbe2f71d194737a81e540e7a345983
janamadhoun <jmadhoun2@gmail.com>
2024-09-19 19:01:37 -0400

    Updated frontend.

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+
+function Header() {
+return (
+<>
+        <div id="header"><br></br><br></br>
+            <h1 id="title">Peer Assessment Program</h1>
+        </div>
+   
+</>
+); 
+}
+export default Header;
