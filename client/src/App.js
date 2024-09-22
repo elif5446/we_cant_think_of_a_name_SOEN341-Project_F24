bef4ff8e1c83938dcf624edf847480eb6d91f2b4
janamadhoun <jmadhoun2@gmail.com>
2024-09-21 22:40:29 -0400

    Instructor login page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/Main';
 import StudentLogin from './components/StudentLogin';
+import InstructorLogin from './components/InstructorLogin';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path = "/" element = {<Main />} />
         <Route path = "/student-login" element = {<StudentLogin />} />
+        <Route path = "/instructor-login" element = {<InstructorLogin />} />
+
 
       </Routes>
     </Router>
--- client/src/components/InstructorLogin.js
+++ client/src/components/InstructorLogin.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../styles/StudentLogin.css'; 
+import '../styles/InstructorLogin.css'; 
 
-const StudentLogin = () => (
+const InstructorLogin = () => (
     
-<div className="student-login-page">
-    <div className="student-login-container">
+<div className="instructor-login-page">
+    <div className="instructor-login-container">
         <img src="/images/loginStudent.svg" alt="Sign Up Icon" className="student-icon" />
         <div id = "login-section"><br></br>
-        <h1 className="maintitle">STUDENT</h1>
+        <h1 className="maintitle">INSTRUCTOR</h1>
         <h1 className="maintitle">LOGIN</h1>
 
 
             <form id="form-group">
                 <div className = "form-group"><br></br>
-                    <label htmlFor="studentid">Student ID:</label><br></br>
+                    <label htmlFor="studentid">Instructor ID:</label><br></br>
                     <input type="text" id="studentid" name="studentid" required />
                 </div>
                 <div className="form-group">
@@ -29,4 +29,4 @@ const StudentLogin = () => (
 </div>
 );
 
-export default StudentLogin;
+export default InstructorLogin;
